04cc36e4f800259aefed93b77a429848bfbba79f
Daniil Goncharov <brovar002@gmail.com>
2023-02-09 15:30:02 +0300

    init: start task

---
+++ src/main/java/by/goncharov/kameleoonTask/KameleoonTaskApplication.java
@@ -0,0 +1,13 @@
+package by.goncharov.kameleoonTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KameleoonTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/goncharov/kameleoonTask/KameleoonTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.goncharov.kameleoonTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
